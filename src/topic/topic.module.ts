e036b22b16c193a631189eaab2b79a7b29c116ef
Максим Минченко <minchenko_ma@nlmk.com>
2022-01-29 23:13:34 +0300

    create api

--- src/topic/topic.module.ts
+++ src/topic/topic.module.ts
@@ -1,8 +1,9 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { TopicController } from './topic.controller';
-import { DayModule } from 'src/day/day.module';
 import { UserModule } from 'src/user/user.module';
+import { GoalModule } from 'src/goal/goal.module';
+import { DayModule } from 'src/day/day.module';
 import { MarkdownService } from 'src/markown/markdown.service';
 import { TopicService } from './topic.service';
 import { Topic } from './topic.entity';
@@ -10,6 +11,6 @@ import { Topic } from './topic.entity';
 @Module({
   controllers: [TopicController],
   providers: [TopicService, MarkdownService],
-  imports: [TypeOrmModule.forFeature([Topic]), UserModule, DayModule],
+  imports: [TypeOrmModule.forFeature([Topic]), UserModule, GoalModule, DayModule],
 })
 export class TopicModule {}
--- src/topic/topic.service.ts
+++ src/topic/topic.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { DayService } from 'src/day/day.service';
-import { UserService } from 'src/user/user.service';
-import { MarkdownService } from 'src/markown/markdown.service';
-import { CreateTopicDto } from './dto/create-topic.dto';
-import { Topic } from './topic.entity';
-import { FindQuery } from './dto/find-query';
 import { FindOneOptions } from 'typeorm/find-options/FindOneOptions';
-import { Answer } from '../answer/answer.entity';
+import { UserService } from 'src/user/user.service';
+import { GoalService } from 'src/goal/goal.service';
+import { DayService } from 'src/day/day.service';
+import { MarkdownService } from 'src/markown/markdown.service';
+import { Answer } from 'src/answer/answer.entity';
+import { CreateTopicDto } from './dto/create-topic.dto';
+import { FindQuery } from './dto/find-query';
+import { Topic } from './topic.entity';
 
 @Injectable()
 export class TopicService {
@@ -16,30 +17,39 @@ export class TopicService {
     @InjectRepository(Topic)
     private readonly topicRepository: Repository<Topic>,
     private readonly userService: UserService,
+    private readonly goalService: GoalService,
     private readonly dayService: DayService,
     private readonly markdownService: MarkdownService,
   ) {}
 
   async create(userId: number, dto: CreateTopicDto) {
     const user = await this.userService.findByPK(userId);
-    const topic = !dto.answer ? new Topic() : await this.findByPK(dto.answer, { relations: ['day'] });
+    const topic = !dto.answer
+      ? new Topic()
+      : await this.findByPK(dto.answer, { relations: ['day', 'day.goal'] });
 
-    if (!dto.answer) {
-      const day = await this.dayService.findByPK(dto.dayId);
-      topic.text = this.markdownService.convert(dto.text);
-      topic.type = dto.type;
-      topic.user = user;
-      topic.day = day;
-    } else {
-      const answer = new Answer();
-      answer.text = this.markdownService.convert(dto.text);
-      answer.user = user;
-      topic.answers.push(answer);
-    }
+    return this.topicRepository.manager.transaction(async (transactionalManager) => {
+      if (!dto.answer) {
+        const day = await this.dayService.findByPK(dto.dayId);
+        topic.text = this.markdownService.convert(dto.text);
+        topic.type = dto.type;
+        topic.user = user;
+        topic.day = day;
+      } else {
+        const goal = await this.goalService.findByPK(topic.day.goal.id, { relations: ['characteristic'] });
+        const answer = new Answer();
+        answer.text = this.markdownService.convert(dto.text);
+        answer.user = user;
+        topic.answers.push(answer);
+        topic.day.characteristic.support += 1;
+        goal.characteristic.support += 1;
+        await transactionalManager.save(goal);
+      }
 
-    topic.day.topicCount += 1;
+      topic.day.topicCount += 1;
 
-    return this.topicRepository.save(topic);
+      return transactionalManager.save(topic);
+    });
   }
 
   async find(query: FindQuery) {

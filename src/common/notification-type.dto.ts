373a6a940fffe903a7f7c48ba3f60a11a9ca6063
Максим Минченко <minchenko_ma@nlmk.com>
2023-08-06 23:29:23 +0300

    feat/app: new mechanics

--- src/common/notification-type.dto.ts
+++ src/common/notification-type.dto.ts
@@ -2,8 +2,6 @@ export enum NotificationTypeDto {
   NewGoal = 'new-goal',
   NewFollower = 'new-follower',
   AddedPoints = 'added-points',
-  AddMotivation = 'add-motivation',
-  AddCreativity = 'add-creativity',
   NewQuestion = 'new-question',
   NewSupport = 'new-support',
   NewAnswer = 'new-answer',

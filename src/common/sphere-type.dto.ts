33a8ac78ea319d35eda616f6f3e2282458601c04
Максим Минченко <minchenko_ma@nlmk.com>
2023-08-11 22:05:42 +0300

    refactor/app: basis

---
+++ src/common/sphere-type.dto.ts
@@ -0,0 +1,10 @@
+export enum SphereType {
+  Family = 'family',
+  Friends = 'friends',
+  Health = 'health',
+  Hobbies = 'hobbies',
+  Money = 'money',
+  Vacation = 'vacation',
+  Development = 'development',
+  Work = 'work',
+}

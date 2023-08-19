b431fac0128069ae23e6c54b9a2a42bcc7705683
Максим Минченко <minchenko_ma@nlmk.com>
2023-08-19 22:42:25 +0300

    feat/app: new mechanics

--- src/user/entities/user-with-characteristic.entity.ts
+++ src/user/entities/user-with-characteristic.entity.ts
@@ -1,9 +1,9 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { OneToOne } from 'typeorm';
 import { UserCharacteristicEntity } from 'src/user-characteristic/entities/user-characteristic.entity';
-import { UserExtendedEntity } from './userExtendedEntity';
+import { UserBaseEntity } from './user-base.entity';
 
-export class UserWithCharacteristicEntity extends UserExtendedEntity {
+export class UserWithCharacteristicEntity extends UserBaseEntity {
   @OneToOne(() => UserCharacteristicEntity, (characteristic) => characteristic.user, { cascade: true })
   @ApiPropertyOptional({ type: () => UserCharacteristicEntity })
   characteristic: UserCharacteristicEntity;
